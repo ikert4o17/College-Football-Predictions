39afd92094628ae3e81374c90d19c6f12000afca
ikert4o17 <bhrzrsyssn@privaterelay.appleid.com>
2026-08-13 10:06:38 -0500

    Create sources.py

---
+++ data/sources.py
@@ -0,0 +1,15 @@
+"""
+External data sources used by the model.
+
+URLs and source identifiers are kept here so they can be
+changed without modifying the rest of the application.
+"""
+
+DATA_SOURCES = {
+    "teams": None,
+    "schedules": None,
+    "games": None,
+    "stats": None,
+    "rankings": None,
+    "odds": None,
+}
